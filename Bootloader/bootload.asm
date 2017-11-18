336300d91f22ac9b68854f1aa7865662ba8f7dab
William V <33563097+campital@users.noreply.github.com>
2017-11-18 16:55:50 -0500

    The bootloader now loads the kernel

--- Bootloader/bootload.asm
+++ Bootloader/bootload.asm
@@ -39,8 +39,9 @@ start:
     mov ax, 0
     mov ds, ax ; reset data segment because we are using ORG
     
-    call PrintString ; print the string
+     ; print the string
     call LoadKernel
+    call PrintString
     call JumpToKernel
     
     jmp $ ; halt the processor, program ended
@@ -57,7 +58,7 @@ PrintString: ; location of string is in bp
     mov ah, 0x0E ; print mode
     mov bl, 0 ; Colors don't seem to be working
     mov bh, 0 ; we dont need to set the page mode
-    int 10h ; print the char
+    int 0x10 ; print the char
     inc cx ; increment the counter
     cmp cx, 200 ; max string length
     je .done
@@ -72,11 +73,53 @@ LoadKernel: ; FirstRootDirSecNum = BPB_ResvdSecCnt (1) + (BPB_NumFATs (2) * BPB_
     mov di, ax ; fix bugs
     mov ah, 8 ; code for get drive info
     clc
-    int 13h
-    jc .error ; was CF set on error
-    ; TODO: CALCULATE SIDE TRACK SECTOR AND LOAD KERNEL TO MEM
+    int 0x13
+    
+    push cx
+    and cx, 0x003F ; isolate the low 6 bits of cx; sectors per track
+    mov [sectors_per_track], cx ; choose the sectors per track
+    pop cx
+    
+    and cx, 0xFFC0
+    shr cx, 6 ; isolate
+    mov [tracks_per_head], cx
+    
+    mov ax, 33 ; 33 is where the data starts
+    xor dx, dx ; set dx to zero for division
+    div word [sectors_per_track] ; get number of sectors
+    and dx, 0x003F ; should already be
+    mov cx, dx
+    
+    xor dx, dx ; set dx to zero
+    cmp ax, 0
+    je .zero
+    div word [tracks_per_head] ; track number is already in ax
+    jmp .normal
+.zero:
+    mov dx, ax ; ax is number of tracks
+    xor ax, ax ; head number if 0 = 0
+.normal:
+    mov bx, dx ; extra register
+    shl bx, 6 ; give space for sector number that is already in cx
+    or cx, bx ; combine the track number into cx
+    mov dh, al ; should move the head number to dh
+    
+    mov al, 16 ; read 16 sectors
+    mov ah, 2 ; read sector
+    ; already have track number
+    ; already have sector number
+    ; already have head number
+    mov dl, [disknumber]
+    xor bx, bx ; set bx to 0
+    mov es, bx ; just to make sure
+    mov bx, 0x7F00 ; where to load the kernel
+    clc
+    int 0x13 ; LOAD THE KERNEL!
+    jc .error
+    ret
+    
 .error:
-    ret ; go back to hlt, error
+    jmp $ ; dont return
     
 ; JumpToKernel: Sets up protected mode and jumps to the kernel at kernel_address
 JumpToKernel:
@@ -84,9 +127,11 @@ JumpToKernel:
     
     
     
-print_str db 'Loading the operating system...', 0 ; 0 is the standard termination
+print_str db 'Loaded the operating system!', 0 ; 0 is the standard termination
 kern_filename db 'KERN      X', 0 ; we will be searching for the short entry in the root directory "FAT"
 disknumber db 0
 kernel_address dw 0x8000 ; load kernel at 32k
+sectors_per_track dw 0
+tracks_per_head dw 0
 times 510 - ($ - $$) db 0 ; allow the boot signature to be in the last two bytes
 dw 0xAA55 ; the boot sig
